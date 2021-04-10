aecfb1d1dc02c6dfc6c3b8bad6c5d16e454b2faa
WahidX <wahidx93@gmail.com>
2021-04-10 20:49:28 +0530

    server init

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const app = require('express')();
+const PORT = process.env.PORT || 8000;
+
+// cors handle
+const cors = require('cors');
+app.use(cors());
+
+const { graphqlHTTP } = require('express-graphql');
+
+// const db = require('./config/mongoose');
+
+// const schema = require('./schema/schema');
+
+app.get('/', (req, res) => {
+  return res.json('Working!');
+});
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    // schema,
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, () => console.log('Now browse to localhost:4000/graphql'));
